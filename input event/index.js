5cc6d86c832d42b135c25f26065d0ff1c91123e8
Gildas <72899528+achmadgildas@users.noreply.github.com>
2023-11-23 08:25:43 +0800

    add OnFocus event

--- input event/index.js
+++ input event/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const inputMaxLengthOnLoad = document.getElementById('inputNama').maxLength;
     document.getElementById('sisaKarakter').innerText = inputMaxLengthOnLoad;
-
+   //event OnInput dan menampilkan jumalah karekater maksimal
     document.getElementById('inputNama').addEventListener('input', function () {
         const jumlahKarakterDiketik = document.getElementById('inputNama').value.length;
         const jumlahKarakterMaksimal = document.getElementById('inputNama').maxLength;
@@ -18,6 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
           document.getElementById('notifikasiSisaKarakter').style.color = 'black';
         }
+      });
+    //event OnFocus, menampilkan sisa karakter
+      document.getElementById('inputNama').addEventListener('focus', function () {
+        console.log('inputNama: focus');
+        document.getElementById('notifikasiSisaKarakter').style.visibility = 'visible';
       });
 
 });
